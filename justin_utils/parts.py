878a25c5e0ccb087cb77eee4c2e9350c3fb813ae
Igor Djachenko <i.s.djachenko@gmail.com>
2022-04-30 03:03:44 +0700

    Developed parts muuuch further

--- justin_utils/parts.py
+++ justin_utils/parts.py
@@ -1,142 +1,231 @@
 import glob
-from argparse import ArgumentParser, Namespace
+import random
+import string
+from abc import ABC, abstractmethod
+from argparse import Namespace
+from dataclasses import dataclass
 from math import log10
 from pathlib import Path
-from typing import Tuple, Callable
+from typing import List, Optional
 
-__SEPARATOR = "."
-__INDEX_START = 1
+from justin_utils.cli import Action, Context, Parameter, App, Command
 
-
-def __is_part(path: Path):
-    split_result = path.name.split(__SEPARATOR)
-
-    return len(split_result) > 0 and split_result[0].isdecimal()
+SEPARATOR = "."
 
 
-def __split_part(path: Path) -> Tuple[int, str]:
-    name_parts = path.name.split(__SEPARATOR, maxsplit=1)
+@dataclass
+class Part:
+    index: int
+    name: Optional[str]
 
-    index = int(name_parts[0])
-    name = name_parts[1] if len(name_parts) == 2 else None
+    path: Path
 
-    return index, name
+    @classmethod
+    def from_path(cls, path: Path) -> 'Part':
+        name_parts = path.name.split(SEPARATOR, maxsplit=1)
 
+        index = int(name_parts[0])
+        name = name_parts[1] if len(name_parts) == 2 else None
 
-def __to_padded_string(number: int, length: int, padding: str) -> str:
-    number_len = (int(log10(number)) if number > 0 else 0) + 1
+        return Part(
+            index=index,
+            name=name,
+            path=path
+        )
 
-    parts = [padding] * (length - number_len) + [str(number)]
+    @staticmethod
+    def is_part(path: Path) -> bool:
+        split_result = path.name.split(SEPARATOR)
 
-    return "".join(parts)
-
-
-def __run_make(args: Namespace):
-    count = args.count
-    pattern: str = args.path
-
-    for str_path in glob.iglob(pattern):
-        path = Path(str_path).absolute()
-
-        make_parts(path, count)
-
-
-def make_parts(path: Path, count: int):
-    existing_subfolders = [f for f in path.iterdir() if f.is_dir()]
-    existing_parts = [f for f in existing_subfolders if __is_part(f)]
-    existing_indices = [__split_part(part)[0] for part in existing_parts]
-
-    max_index_length = int(log10(count)) + 1
-
-    for index in range(count):
-        index += __INDEX_START
-
-        if index in existing_indices:
-            continue
-
-        part_name = __to_padded_string(index, max_index_length, "0")
-
-        part_path = path / part_name
-
-        assert not part_path.exists()
-
-        part_path.mkdir(parents=True)
+        return len(split_result) > 0 and split_result[0].isdecimal()
 
 
-def __run_renumber(args: Namespace):
-    pattern: str = args.path
-
-    for str_path in glob.iglob(pattern):
-        path = Path(str_path).absolute()
-
-        renumber_parts(path)
+# region main funcs
 
 
-def renumber_parts(path: Path):
-    existing_subfolders = [f for f in path.iterdir() if f.is_dir()]
-    existing_parts = [f for f in existing_subfolders if __is_part(f)]
+class PartsAction(Action, ABC):
+    INDEX_START = 1
 
-    parts_count = len(existing_parts)
+    @property
+    def parameters(self) -> List[Parameter]:
+        return super().parameters + [
+            Parameter("root", nargs="?", default="."),
+        ]
 
-    if parts_count < 1:
-        print(f"There are no parts at {path}")
+    def perform(self, args: Namespace, context: Context) -> None:
+        pattern: str = args.root
 
-        return
+        for str_path in glob.iglob(pattern):
+            path = Path(str_path).absolute()
+            parts = self.get_parts(path)
 
-    # noinspection PyTypeChecker
-    parts = [__split_part(part) + (part,) for part in existing_parts]
+            self.perform_for_root(path, parts, args)
 
-    parts.sort(key=lambda x: x[0])
+    @abstractmethod
+    def perform_for_root(self, root: Path, parts: List[Part], args: Namespace):
+        pass
 
-    max_index_length = int(log10(parts_count)) + 1
+    @staticmethod
+    def get_parts(path: Path) -> List[Part]:
+        existing_subfolders = [f for f in path.iterdir() if f.is_dir()]
+        existing_parts = [f for f in existing_subfolders if Part.is_part(f)]
+        struct_parts = [Part.from_path(part) for part in existing_parts]
 
-    tmp_parts = []
+        return struct_parts
 
-    for index, name, path in parts:
-        tmp_path = path.with_name(f"tmp_{index}")
+    @staticmethod
+    def dump_in_temp(root: Path, parts: List[Part]) -> List[Part]:
+        while True:
+            tmp_folder_name = "".join(random.choices(string.digits + string.ascii_letters, k=10))
 
-        path.rename(tmp_path)
+            tmp_folder_path = root / tmp_folder_name
 
-        tmp_parts.append((index, name, tmp_path))
+            if not tmp_folder_path.exists():
+                break
 
-    parts = tmp_parts
+        tmp_folder_path.mkdir(parents=True)
 
-    for index, t in enumerate(parts):
-        _, name, path = t
+        tmp_parts = []
 
-        index += __INDEX_START
+        for part in parts:
+            tmp_path = tmp_folder_path / part.path.name
 
+            part.path.rename(tmp_path)
+
+            tmp_parts.append(Part.from_path(tmp_path))
+
+        return tmp_parts
+
+    @staticmethod
+    def to_padded_string(number: int, length: int, padding: str) -> str:
+        number_len = (int(log10(number)) if number > 0 else 0) + 1
+
+        parts = [padding] * (length - number_len) + [str(number)]
+
+        return "".join(parts)
+
+    @staticmethod
+    def new_part_name(part: Part, new_index: int, max_index_length: int) -> str:
         new_name_parts = [
-            __to_padded_string(index, max_index_length, "0"),
-            name
+            PartsAction.to_padded_string(new_index, max_index_length, "0"),
+            part.name
         ]
 
         new_name_parts = [i for i in new_name_parts if i]
+        new_name = SEPARATOR.join(new_name_parts)
 
-        new_name = __SEPARATOR.join(new_name_parts)
+        return new_name
 
-        new_path = path.with_name(new_name)
+    @staticmethod
+    def index_length(index: int) -> int:
+        if index == 0:
+            return 1
+        return int(log10(index)) + 1
 
-        path.rename(new_path)
+
+class MakeAction(PartsAction):
+    @property
+    def parameters(self) -> List[Parameter]:
+        return super().parameters + [
+            Parameter("count", type=int)
+        ]
+
+    def perform_for_root(self, root: Path, parts: List[Part], args: Namespace):
+        count = args.count
+
+        existing_indices = {part.index for part in parts}
+
+        max_index_length = self.index_length(count)
+
+        for index in range(count):
+            index += self.INDEX_START
+
+            if index in existing_indices:
+                continue
+
+            part_name = self.to_padded_string(index, max_index_length, "0")
+
+            part_path = root / part_name
+
+            assert not part_path.exists()
+
+            part_path.mkdir(parents=True)
+
+
+class RenumberAction(PartsAction):
+
+    @property
+    def parameters(self) -> List[Parameter]:
+        return super().parameters + [
+            Parameter(flags=["-w", "--width"], type=int, default=None)
+        ]
+
+    def perform_for_root(self, root: Path, parts: List[Part], args: Namespace):
+
+        parts_count = len(parts)
+
+        parts.sort(key=lambda x: x.index)
+
+        parts = self.dump_in_temp(root, parts)
+
+        max_index_length = self.index_length(parts_count)
+
+        if args.width is not None:
+            max_index_length = max(max_index_length, args.width)
+
+        for index, part in enumerate(parts, start=RenumberAction.INDEX_START):
+            new_name = self.new_part_name(part, index, max_index_length)
+
+            new_path = root / new_name
+
+            part.path.rename(new_path)
+
+
+class OffsetAction(PartsAction):
+
+    @property
+    def parameters(self) -> List[Parameter]:
+        return super().parameters + [
+            Parameter("offset", type=int),
+            Parameter(flags=["-w", "--width"], type=int, default=None),
+        ]
+
+    def perform_for_root(self, root: Path, parts: List[Part], args: Namespace):
+        parts = self.get_parts(root)
+
+        if not parts:
+            return
+
+        offset: int = args.offset
+
+        indices = [part.index for part in parts]
+
+        assert min(indices) + offset >= 0
+
+        max_index = max(indices) + offset
+        max_index_length = self.index_length(max_index)
+
+        if args.width is not None:
+            max_index_length = max(max_index_length, args.width)
+
+        parts = self.dump_in_temp(root, parts)
+
+        for part in reversed(parts):
+            new_index = part.index + offset
+
+            new_name = self.new_part_name(part, new_index, max_index_length)
+
+            new_path = root / new_name
+
+            part.path.rename(new_path)
+
+
+# endregion main funcs
 
 
 def __run():
-    parser = ArgumentParser()
-
-    adder = parser.add_subparsers()
-
-    make_parser = adder.add_parser("make")
-    renumber_parser = adder.add_parser("renumber")
-
-    for p in [make_parser, renumber_parser]:
-        p.add_argument("path", type=str, nargs="?", default=".")
-
-    make_parser.add_argument("count", type=int)
-    make_parser.set_defaults(func=__run_make)
-
-    renumber_parser.set_defaults(func=__run_renumber)
-
-    namespace = parser.parse_args()
-
-    if hasattr(namespace, "func") and namespace.func and isinstance(namespace.func, Callable):
-        namespace.func(namespace)
+    App([
+        Command("make", [MakeAction()]),
+        Command("renumber", [RenumberAction()]),
+        Command("offset", [OffsetAction()])
+    ]).run()
