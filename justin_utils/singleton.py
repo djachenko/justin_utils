b1c134febdb59e3fb12c28b619fd677d59bfb2d0
Igor Djachenko <i.s.djachenko@gmail.com>
2022-10-08 22:06:18 +0700

    Added guards for initializing not from instance

--- justin_utils/singleton.py
+++ justin_utils/singleton.py
@@ -6,7 +6,20 @@ T = TypeVar('T', bound='Singleton')
 
 
 class Singleton(ABC):
+    __initiating_from_instance = False
+
+    def __init__(self) -> None:
+        assert self.__initiating_from_instance
+
+        super().__init__()
+
     @classmethod
     @lru_cache()
     def instance(cls: Type[T]) -> T:
-        return cls()
+        cls.__initiating_from_instance = True
+
+        instance = cls()
+
+        cls.__initiating_from_instance = False
+
+        return instance
